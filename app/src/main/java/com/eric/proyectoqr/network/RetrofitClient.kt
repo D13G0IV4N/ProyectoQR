1eecc8246cc202167c0a39eb6339b207d3a66d00
Diego Ivan <diegoivan1233@gmail.com>
2025-11-11 19:10:45 -0600

    Funciona para conectarse a la web con emulador (No en el celular)

--- app/src/main/java/com/eric/proyectoqr/network/RetrofitClient.kt
+++ app/src/main/java/com/eric/proyectoqr/network/RetrofitClient.kt
@@ -6,12 +6,14 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://defined-literacy-processor-vice.trycloudflare.com/api/"
+
+    private const val BASE_URL = "http://10.0.2.2:8000/api/"
 
     val apiService: ApiService by lazy {
         val loggingInterceptor = HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         }
+
         val httpClient = OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .build()
